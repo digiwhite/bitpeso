7d29aa5598aa47cab163cb14c309fd5b8497f182
digiwhite <jr.white@yandex.com>
2014-05-05 05:27:32 +0300

    fix config file name

--- src/util.cpp
+++ src/util.cpp
@@ -1099,7 +1099,7 @@ const boost::filesystem::path &GetDataDir(bool fNetSpecific)
 
 boost::filesystem::path GetConfigFile()
 {
-    boost::filesystem::path pathConfigFile(GetArg("-conf", "coin.conf"));
+    boost::filesystem::path pathConfigFile(GetArg("-conf", "bitpeso.conf"));
     if (!pathConfigFile.is_complete()) pathConfigFile = GetDataDir(false) / pathConfigFile;
     return pathConfigFile;
 }
